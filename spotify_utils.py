5ecce553dfe05d4fdc42e4631a23db2a7c6bbdf5
MattWis <matthew.wismer@students.olin.edu>
2014-08-27 09:09:05 -0400

    Whitespace

--- spotify_utils.py
+++ spotify_utils.py
@@ -13,36 +13,36 @@ def Spotify(profile = None):
   return _spotify
 
 class _Spotify:
-  
+
   def play_track(self, name):
     runner = lambda x: self._play(self._search(x, "tracks"))
     start_new_thread(runner, (name, ))
-  
+
   def play_album(self, name):
     runner = lambda x: self._play_tracks(self._get_tracks(x, "albums"))
     start_new_thread(runner, (name, ))
-  
+
   def shuffle_album(self, name):
     runner = lambda x: self._shuffle_seq(self._get_tracks(x, "albums"))
     start_new_thread(runner, (name, ))
-  
+
   def play_artist(self, name):
     runner = lambda x: self._play_tracks(self._get_tracks(x, "artists"))
     start_new_thread(runner, (name, ))
-  
+
   def shuffle_artist(self, name):
     runner = lambda x: self._shuffle_seq(self._get_tracks(x, "artists"))
     start_new_thread(runner, (name, ))
-  
+
   def _play(self, track):
     self.session.player.load(track)
     self.session.player.play()
-  
+
   #Returns best search result
   #category is "tracks", "artists", or "albums"
   def _search(self, term, category):
     return getattr(self.session.search(term).load(), category)[0].load()
-  
+
   def _get_tracks(self, name, category):
     grouping = self._search(name, category)
     return grouping.browse().load().tracks
@@ -73,16 +73,16 @@ class _Spotify:
     session = spotify.Session()
     loop = spotify.EventLoop(session)
     loop.start()
-    
+
     # Connect an audio sink
     audio = spotify.PortAudioSink(session)
-    
+
     def on_login(session):
       if session.connection.state is spotify.ConnectionState.LOGGED_IN:
         logged_in.set()
     def on_end_of_track(self):
       end_of_track.set()
-    
+
     # Register event listeners
     logged_in = threading.Event()
     end_of_track = threading.Event()
